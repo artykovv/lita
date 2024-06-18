d8a69b359fc0c1c8f99ced5eb9ce4a83a1439aaa
artykovv <pipzdty@gmail.com>
2024-06-18 14:38:14 +0600

    ver2

--- config.py
+++ config.py
@@ -4,11 +4,11 @@ import os
 
 load_dotenv()
 
-DB_HOST = os.environ.get("DB_HOST")
-DB_PORT = os.environ.get("DB_PORT")
-DB_NAME = os.environ.get("DB_NAME")
-DB_USER = os.environ.get("DB_USER")
-DB_PASS = os.environ.get("DB_PASS")
+DB_HOST = os.environ.get("POSTGRESQL_HOST")
+DB_PORT = os.environ.get("POSTGRESQL_PORT")
+DB_NAME = os.environ.get("POSTGRESQL_DBNAME")
+DB_USER = os.environ.get("POSTGRESQL_USER")
+DB_PASS = os.environ.get("POSTGRESQL_PASSWORD")
 
 PrivateKey = "certs/private.pem"
 PublicKey = "certs/public.pem"
